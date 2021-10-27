3d5db7a0c792553baeb5f626db8892c2043718fc
SeonHakJi <jshag90@naver.com>
2021-10-27 23:45:38 +0900

    [Add] generate 메서드 예제, generate 기반 피보나치 수 생성 예제

---
+++ MJIAPrj/src/com/ji/example/GenerateMethod.java
@@ -0,0 +1,17 @@
+package com.ji.example;
+
+import java.util.stream.Stream;
+
+/**
+ * generate 메서드
+ * @author ji
+ *
+ */
+public class GenerateMethod {
+
+	public static void main(String[] args) {
+		Stream.generate(Math::random)
+		.limit(5)
+		.forEach(System.out::println);
+	}
+}
--- MJIAPrj/src/com/ji/quiz/Fibbonachi.java
+++ MJIAPrj/src/com/ji/quiz/Fibbonachi.java
@@ -1,5 +1,6 @@
 package com.ji.quiz;
 
+import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -14,6 +15,22 @@ public class Fibbonachi {
 		IntStream.iterate(0, n -> n+4)
 		.takeWhile(n -> n < 100)
 		.forEach(System.out::println);
+		
+		IntSupplier fib = new IntSupplier() {
+			private int previous = 0;
+			private int current =  1;
+			
+			@Override
+			public int getAsInt() {
+				int oldPrevious = this.previous;
+				int nextValue = this.previous + this.current;
+				this.previous = this.current;
+				this.current = nextValue;
+				return oldPrevious;
+			}
+		};
+		
+		IntStream.generate(fib).limit(10).forEach(System.out::println);
 
 	}
 
