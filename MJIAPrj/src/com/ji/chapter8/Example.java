2542a503facb61bc13ac53a94648f9659db5b68e
SeonHakJi <jshag90@naver.com>
2021-11-09 21:55:22 +0900

    [Add] 8.3.1 forEach 메서드

--- MJIAPrj/src/com/ji/chapter8/Example.java
+++ MJIAPrj/src/com/ji/chapter8/Example.java
@@ -11,6 +11,10 @@ public class Example {
 		//8.1.3 맵 팩토리
 		Map<String, Integer> ageOfFriends = Map.ofEntries(entry("Raphael", 30), entry("Olivia", 25), entry("Thibaut", 26));
 		System.out.println(ageOfFriends);
+		
+		//8.3.1 forEach메서드
+		ageOfFriends.forEach((friend, age) -> System.out.println(friend + " is "+ age + " years old"));
+		
 	}
 
 }
