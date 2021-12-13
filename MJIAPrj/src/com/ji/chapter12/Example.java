7bb5312fec556e920040365a019b01dae6937a8f
SeonHakJi <jshag90@naver.com>
2021-12-13 20:58:13 +0900

    [Add] chapter 12 DateTimeFormatter 예제

--- MJIAPrj/src/com/ji/chapter12/Example.java
+++ MJIAPrj/src/com/ji/chapter12/Example.java
@@ -2,7 +2,11 @@ package com.ji.chapter12;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAdjusters;
+import java.util.Locale;
 
 public class Example {
 	
@@ -15,6 +19,31 @@ public class Example {
 		LocalDate date3 = date2.with(TemporalAdjusters.lastDayOfMonth());
 		System.out.println(date3);
 
+		// 12-10 패턴으로 DateTimeFormatter 만들기
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate dateFormatter1 = LocalDate.of(2014, 3, 18);
+		String formattedDate = dateFormatter1.format(formatter);
+		LocalDate dateFormatter2 = LocalDate.parse(formattedDate, formatter);
+		System.out.println(dateFormatter2);
+		
+		// 12-11 지역화된 DateTimeFormatter 만들기
+		DateTimeFormatter italianFormatter = DateTimeFormatter.ofPattern("d. MMMM yyyy", Locale.ITALIAN);
+		LocalDate date1It = LocalDate.of(2014, 3, 18);
+		String formattedDateIt = date1It.format(italianFormatter);
+		LocalDate date2It = LocalDate.parse(formattedDateIt, italianFormatter);
+		System.out.println(date2It);
+		
+		// 12-12 DateTimeFormatter 만들기
+		DateTimeFormatter italianFormatterBuilder = new DateTimeFormatterBuilder().appendText(ChronoField.DAY_OF_MONTH)
+				.appendLiteral(". ")
+				.appendText(ChronoField.MONTH_OF_YEAR)
+				.appendLiteral(" ")
+				.appendText(ChronoField.YEAR)
+				.parseCaseInsensitive()
+				.toFormatter(Locale.ITALIAN);
+		
+		LocalDate date2ItBuilder = LocalDate.parse(formattedDateIt, italianFormatterBuilder);
+		System.out.println(date2ItBuilder);
 	}
 	
 }
