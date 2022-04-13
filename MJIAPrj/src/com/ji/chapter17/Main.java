3c93a03aeb9466ddf1788d9413ea9f6fdd804d13
SeonHakJi <jshag90@naver.com>
2022-04-13 23:42:38 +0900

    [Add] 화씨를 섭씨로 변환하는 Processor 추가, Publisher를 만들고 TempSubscriber를 구독시킴

--- MJIAPrj/src/com/ji/chapter17/Main.java
+++ MJIAPrj/src/com/ji/chapter17/Main.java
@@ -5,10 +5,19 @@ import java.util.concurrent.Flow.Publisher;
 public class Main {
 
 	public static void main(String[] args) {
-		getTemperatures("New York").subscribe(new TempSubscriber());
+//		getTemperatures("New York").subscribe(new TempSubscriber());
+		getCelsiusTemperatures("New York").subscribe(new TempSubscriber());
 	}
 
 	private static Publisher<TempInfo> getTemperatures(String town) {
 		return Subscriber -> Subscriber.onSubscribe(new TempSubscription(Subscriber, town));
+	}
+
+	public static Publisher<TempInfo> getCelsiusTemperatures(String town) {
+		return Subscriber -> {
+			TempProcessor processor = new TempProcessor();
+			processor.subscribe(Subscriber);
+			processor.onSubscribe(new TempSubscription(processor, town));
+		};
 	}
 }
--- MJIAPrj/src/com/ji/chapter17/TempSubscription.java
+++ MJIAPrj/src/com/ji/chapter17/TempSubscription.java
@@ -1,13 +1,16 @@
 package com.ji.chapter17;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Subscription;
 
-public class TempSubscription implements Subscription{
-	
+public class TempSubscription implements Subscription {
+
 	private final Subscriber<? super TempInfo> subscriber;
 	private final String town;
-	
+
+	private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
 	public TempSubscription(Subscriber<? super TempInfo> subscriber, String town) {
 		super();
@@ -17,14 +20,20 @@ public class TempSubscription implements Subscription{
 
 	@Override
 	public void request(long n) {
-		for(long i = 0L; i < n; i++) {
-			try {
-				subscriber.onNext(TempInfo.fetch(town));
-			}catch (Exception e) {
-				subscriber.onError(e);
-				break;
+
+		executor.submit(() -> {
+
+			for (long i = 0L; i < n; i++) {
+				try {
+					subscriber.onNext(TempInfo.fetch(town));
+				} catch (Exception e) {
+					subscriber.onError(e);
+					break;
+				}
 			}
-		}
+
+		});
+
 	}
 
 	@Override
